e7f1917221094008bc6707ef6bbfde8464ae52cd
Damien Young <dyoung1006@gmail.com>
2024-05-18 12:16:36 -0700

    hopefully this works

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Given Starting Solution
+            /*
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + "is less than nine";
+            }
+            else 
+            {
+                response = answer + "is greater or equal to nine";
+            }
+            */
+
+            /*
+             * Given the following code, change the code to implement:
+                inferred typing
+                string interpolation
+                and the ternary operator
+             */
+            #endregion
+
+            var answer = 4;  //inferred typing
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";   //$"{answer} is less than..." is string interpolation  and Tenanary operator less than statement  using ? :
+            Console.WriteLine(response);  
+            Console.ReadLine();
         }
     }
 }
